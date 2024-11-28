9321398723733fab8fa3518b9838141162c58eda
neelima.t <neelima.t0510@gmail.com>
2024-11-28 18:14:37 +0530

    shell-script

--- frontend.sh
+++ frontend.sh
@@ -54,6 +54,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/expense-project/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx
 
